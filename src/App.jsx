f9901f71e167fba48e2308719fb714f5acaf3d8f
shakeds5 <Shakeds5598@gmail.com>
2021-11-06 14:17:52 +0200

    configure a specific version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import { EsriMap } from "./components/esri-map/esri-map";
 
+import { setDefaultOptions } from "esri-loader";
+// configure esri-loader to use a specific version from the ArcGIS CDN
+setDefaultOptions({ version: "4.21" });
+
 function App() {
   return (
     <div className="App">
